d4af07ba3126ce344e44672254f3f98f3e6febc5
cc-21 <55864124+cc-21@users.noreply.github.com>
2019-09-27 21:06:01 -0400

    Update Counter.vue

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,13 +1,22 @@
 <template>
-  <p>I'm a Counter!</p>
+  <div>
+    <p>Episodes seen: {{count}} times!</p>
+    <button v-on:click="count += 1">Add</button>
+    <button v-on:click="count -= 1">Delete</button>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
+
   data() {
-    return {
-      count: 0
-    };
+    return count;
   }
 };
 </script>
